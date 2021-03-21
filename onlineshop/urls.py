5796ac914f118862595f61faf560f307da915d49
Ilya <forwow21@mail.ru>
2021-03-21 11:48:50 +0300

    static

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -17,8 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 
 urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
@@ -31,5 +29,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
